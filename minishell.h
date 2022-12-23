a8cae961b137ac264d6ad04762f08b03a6c90a42
Kossivi Gilbert Atchoglo <katchogl@2-C-4.42heilbronn.de>
2022-12-23 20:41:53 +0100

    Initial commit

---
+++ minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katchogl <katchogl@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/23 05:44:06 by katchogl          #+#    #+#             */
+/*   Updated: 2022/12/23 20:41:32 by katchogl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdio.h>
+# include <string.h>
+# include <errno.h>
+# include <stdio.h>
+# include <unistd.h>
+# include "libft/libft.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif
