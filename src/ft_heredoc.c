e9ecc8cea663aed96b38bdd6b651f7a5be937b61
Kossivi Gilbert Atchoglo <katchogl@2-B-5.42heilbronn.de>
2023-01-07 19:50:02 +0100

    edited heredoc

--- src/ft_heredoc.c
+++ src/ft_heredoc.c
@@ -6,26 +6,31 @@
 /*   By: katchogl <katchogl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 17:41:10 by katchogl          #+#    #+#             */
-/*   Updated: 2023/01/07 18:13:43 by katchogl         ###   ########.fr       */
+/*   Updated: 2023/01/07 19:44:42 by katchogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	ft_write(int fd, char *heredoc_lim, char *buff, int count)
+static bool ft_isheredoc_lim(char *heredoc_lim, char *buff)
 {
-	char	*lim;
-
-	lim = ft_strnstr (buff, heredoc_lim, count);
-	if (lim != NULL && *(lim - 1) == '\n'
-		&& *(lim + ft_strlen (heredoc_lim) - 1) == '\n'
-		&& *(lim + ft_strlen (heredoc_lim)) == '\0')
+	char	*heredoc_limendl;
+		
+	if (heredoc_lim != NULL && buff != NULL)
 	{
-		count = 0;
-		while (buff[count] != heredoc_lim[0])
-			count++;
+		heredoc_limendl = ft_strjoin (heredoc_lim, "\n");
+		if (heredoc_limendl != NULL)
+		{
+			if (ft_strncmp (heredoc_limendl, buff,
+				ft_strlen (heredoc_limendl) + 1) == 0)
+			{
+				free (heredoc_lim);
+				return (true);
+			}
+			free (heredoc_limendl);
+		}
 	}
-	write (fd, buff, count);
+	return (false);
 }
 
 static void	ft_read(int fd, char *heredoc_lim)
@@ -35,15 +40,15 @@ static void	ft_read(int fd, char *heredoc_lim)
 
 	while (1)
 	{
-		ft_putstr_fd ("> ", STDOUT_FILENO);
+		ft_putstr_fd ("> ", STDERR_FILENO);
 		count = read (STDIN_FILENO, buff, BUFFER_SIZE - 1);
 		if (count <= 0)
 			break ;
 		else
 		{
 			buff[count] = '\0';
-			if (ft_strncmp (buff, heredoc_lim, count) != 0)
-				ft_write (fd, heredoc_lim, buff, count);
+			if (!ft_isheredoc_lim (heredoc_lim, buff))
+				write (fd, buff, count);
 			else
 				break ;
 		}
@@ -54,6 +59,8 @@ t_fd	ft_heredoc(t_data *data, char *heredoc_lim)
 {
 	t_fd	fd;
 
+	if (heredoc_lim == NULL)
+		return (-1);
 	fd = open ("temp.tmp", O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (fd == -1)
 		ft_throw (data, ERR_FAIL, "file write", true);
