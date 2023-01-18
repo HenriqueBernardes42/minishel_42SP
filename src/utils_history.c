6c6a95f83cdd799a68ac6126f5aeee7787416091
AtchogloDev <gilbert.devt@gmail.com>
2023-01-18 13:33:15 +0100

    Update utils_history.c

--- src/utils_history.c
+++ src/utils_history.c
@@ -6,7 +6,7 @@
 /*   By: katchogl <katchogl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 13:04:58 by katchogl          #+#    #+#             */
-/*   Updated: 2023/01/18 13:21:58 by katchogl         ###   ########.fr       */
+/*   Updated: 2023/01/18 13:32:13 by katchogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,15 @@ void	ft_remove_last_history(t_data *data)
 	
 	if (data->history == NULL || data->where_history == 0)
 		return ;
-	nhistory = (char **) malloc
-		(data->where_history * sizeof (char *));
-	if (nhistory == NULL)
-		ft_throw (data, ERR_NULL_PTR, NULL, true);
+	nhistory = NULL;
 	i = -1;
 	clear_history ();
 	data->where_history--;
 	while (++i < data->where_history)
 	{
-		nhistory[i] = ft_strdup (data->history[i]);
+		ft_push (data, &nhistory, data->history[i]);
 		add_history (data->history[i]);
 	}
-	nhistory[i] = NULL;
 	ft_destroy_tab (data->history);
 	data->history = nhistory;
 }
