1a51d72ee3d312582a844ed982519e3cafed4256
hhenriqu <hhenriqu@student.42sp.org.br>
2023-04-15 19:16:20 +0200

    norminette ok

--- src/quotes.c
+++ src/quotes.c
@@ -6,7 +6,7 @@
 /*   By: hhenriqu <hhenriqu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 02:06:42 by hhenriqu          #+#    #+#             */
-/*   Updated: 2023/04/15 16:32:49 by hhenriqu         ###   ########.fr       */
+/*   Updated: 2023/04/15 19:15:35 by hhenriqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,17 @@ int	ft_single_quotes(char *line)
 	open = 0;
 	while (line[i] != '\0')
 	{
-		if ((line[i] == '\"' || line[i] == '\'') && open == 0 )
+		if ((line[i] == '\"' || line[i] == '\'') && open == 0)
 		{
 			quote = line[i];
 			open++;
 			i++;
 		}
 		else if (line[i] == quote && open == 1)
-			{
+		{
 			open = 0;
 			i++;
-			}
+		}
 		else
 			i++;
 	}
